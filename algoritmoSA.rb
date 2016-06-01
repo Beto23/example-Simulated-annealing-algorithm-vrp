efdaa8d51fc70bb4db53923598b9b0af7a6ab691
Beto23 <josee-45@hotmail.com>
2016-06-01 12:41:16 -0500

    Agregar haversine para medir distancias entre dos puntos de la tierra

---
+++ algoritmoSA.rb
@@ -0,0 +1,55 @@
+require 'haversine'
+
+tienda1 = {
+  :nombre => "oxxo odontologia",
+  :latitud => 25.448665,
+  :longitud => -101.025080,
+  :cantidad => 200
+}
+
+tienda2 = {
+  :nombre => "oxxo aquiles velas",
+  :latitud => 25.453243,
+  :longitud => -101.025315,
+  :cantidad => 350
+}
+
+tienda3 = {
+  :nombre => "oxxo calz Madero",
+  :latitud => 25.444569,
+  :longitud => -101.022286,
+  :cantidad => 400
+}
+
+tienda4 = {
+  :nombre => "oxxo sanJose",
+  :latitud => 25.455042,
+  :longitud => -101.028325,
+  :cantidad => 560
+}
+
+tienda5 = {
+  :nombre => "oxxo Jupiter",
+  :latitud => 25.458210,
+  :longitud =>  -101.018548,
+  :cantidad => 255
+}
+
+tienda6 = {
+  :nombre => "oxxo P.Insurgente",
+  :latitud => 25.454832,
+  :longitud =>  -101.010126,
+  :cantidad => 175
+}
+tienda7 = {
+  :nombre => "oxxo L.Echeverria",
+  :latitud => 25.455705,
+  :longitud =>  -101.010151,
+  :cantidad => 387
+}
+deposito = {
+  :nombre => "Bimbo",
+  :latitud => 25.4561015,
+  :longitud =>  -101.0166124,
+  :cantidad => 0
+}
